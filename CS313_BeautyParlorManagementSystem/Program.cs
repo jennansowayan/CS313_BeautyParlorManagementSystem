48a8d026cd1a7d9a484ce7a8cb588d5503a0f2b7
jennansowayan <42040549+jennansowayan@users.noreply.github.com>
2019-11-20 23:21:48 +0300

    added class salon

---
+++ CS313_BeautyParlorManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS313_BeautyParlorManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CS313_BeautyParlorManagementSystem/Salon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CS313_BeautyParlorManagementSystem
+{
+    public class Salon
+    {
+        public Salon()
+        {
+        }
+    }
+}

d9fbccdb401ba596323aa917b2f93a3b087cf3a2
raghad133 <58010294+raghad133@users.noreply.github.com>
2019-11-26 15:12:31 +0300

    Update User.cs

--- CS313_BeautyParlorManagementSystem/User.cs
+++ CS313_BeautyParlorManagementSystem/User.cs
@@ -1,10 +1,135 @@
 ﻿using System;
 namespace CS313_BeautyParlorManagementSystem
+
 {
-    public class User
+    abstract class user
     {
-        public User()
+        public string UserName
         {
+            get;
+            private set;
+        }
+        public string password
+        {
+            get;
+            private set;
+        }
+        public user(string UserName, string password)
+        {
+            this.UserName = UserName;
+            this.password = password;
+        }
+        public abstract string identified();
+
+        //class manager.
+        public class manager : user
+        {
+            public string fristName
+            {
+                get;
+                private set;
+            }
+            public string LastName
+            {
+                get;
+                private set;
+            }
+            public string SalonWork
+            {
+                get;
+                private set;
+            }
+            public manager(string fristname, string Lastname, string SalonWork, string UserName, string password)
+                : base(UserName, password)
+            {
+                this.LastName = LastName;
+                this.fristName = fristName;
+                this.SalonWork = SalonWork;
+            }
+            public override string identified(){
+{
+             throw new NotImplementedException();
+}
+           
+            }
+            // class staff
+            public class staff : user
+            {
+                public string fristName
+            {
+                get;
+                private set;
+            }
+            public string LastName
+            {
+                get;
+                private set;
+            }
+            public string SalonWork
+            {
+                get;
+                private set;
+            }
+            public string StaffMem
+            {
+                get;
+                private set;
+            }
+            public string Service
+            {
+                get;
+                private set;
+            }
+            public staff(string firstName, string LastName, string SalonWork, string StaffMem, string Service
+                , string UserName, string password)
+                : base(UserName, password)
+            {
+                this.fristName = fristName;
+                this.LastName = LastName;
+                this.SalonWork = SalonWork;
+                this.StaffMem = StaffMem;
+                this.Service = Service;
+            }
+            public override string identified()
+            {
+                throw new NotImplementedException();
+            }
+            }
+            //class customer
+            public class customer : user
+            {
+                    public string fristName
+                    {
+                        get;
+                        private set;
+                    }
+                    public string LastName
+                    {
+                        get;
+                        private set;
+                    }
+                    public string PhoneNum
+                    {
+                        get;
+                        private set;
+                    }
+                    public customer(string fristname, string Lastname, string PhoneNum, string UserName, string password)
+                        : base(UserName, password)
+                    {
+                        this.LastName = LastName;
+                        this.fristName = fristName;
+                        this.PhoneNum = PhoneNum;
+                    }
+                    public override string identified()
+                    {
+                        {
+                            throw new NotImplementedException();
+                        }
+                    }
+            }
+            static void Main(string[] args)
+            {
+            }
         }
     }
-}
+    }

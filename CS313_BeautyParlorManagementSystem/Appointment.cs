bf889fe7237cb0ff01c5f3e73202d3a8b015b78d
jennansowayan <42040549+jennansowayan@users.noreply.github.com>
2019-11-30 17:43:19 +0300

    Update Appointment.cs

--- CS313_BeautyParlorManagementSystem/Appointment.cs
+++ CS313_BeautyParlorManagementSystem/Appointment.cs
@@ -3,8 +3,36 @@ namespace CS313_BeautyParlorManagementSystem
 {
     public class Appointment
     {
-        public Appointment()
-        {
-        }
-    }
+		private DateTime DT;
+		private int numOfappointment;
+		private customer appCustomer;
+		private staff Stylist;
+		private Salon appSalon;
+		private Service appservice;
+
+
+		public appointment(DateTime dt, int Numopp, customer NOC,
+						   staff NOStylest, Salon NOSalon, Service TOServicee)
+		{
+			DT = dt;
+			NumOfappointment = Numopp;
+			AppCustomer = NOC;
+			Stylist = NOStylest;
+			appSalo = NOSalon;
+			Appservice = TOServicee;
+		}
+		public DateTime dt
+		{
+			get
+			{
+				return DT;
+			}
+			set => DT = DateTime.Now;
+		}
+
+		public customer AppCustomer { get => appCustomer; set => appCustomer = value; }
+		public staff Stylist { get => Stylist; set => Stylist = value; }
+		public Salon AppSalon { get => appSalon; set => appSalon = value; }
+		public Service Appservice { get => appservice; set => appservice = value; }
+	}
 }
